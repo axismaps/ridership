48a5215e3b305ec57a6ce9e6fa8c8c524d44fe63
David Heyman <dave@axismaps.com>
2018-11-04 12:14:41 +0000

    Make the tiles script easier to read

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -1,3 +1,11 @@
-mapshaper data/geojson/transit/**/routes.geojson combine-files -merge-layers force -filter-fields color,high_frequency,name,operated_by_name,tags,vehicle_type -o data/output/routes.geojson
-mapshaper data/geojson/transit/**/stops.geojson combine-files -merge-layers force -filter-fields high_frequency,name,tags -o data/output/stops.geojson
+mapshaper data/geojson/transit/**/routes.geojson combine-files \
+  -merge-layers force \
+  -filter-fields color,high_frequency,name,operated_by_name,tags,vehicle_type \
+  -o data/output/routes.geojson
+
+mapshaper data/geojson/transit/**/stops.geojson combine-files \
+  -merge-layers force \
+  -filter-fields high_frequency,name,tags \
+  -o data/output/stops.geojson
+
 tippecanoe data/output/*.geojson -aD -aG -ab -ai -f -o data/output/transit.mbtiles
