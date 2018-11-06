e07bae799b660fbc837f5c2409a750b71fe5c743
David Heyman <dave@axismaps.com>
2018-11-06 12:52:57 +0000

    Write TAs and MSAs to CSVs

--- bin/meta.py
+++ bin/meta.py
@@ -1,5 +1,4 @@
 import re
-import json
 import pandas as pd
 import geopandas as gpd
 
@@ -8,9 +7,9 @@ def clean_ta(ta, drop):
     ta = ta.dropna(how='all')
 
     # Combine project ID's
-    # ta['Project ID'] = ta['Project ID'].combine_first(
-    #     ta['"Other" primary Project ID']
-    # ).astype('int32')
+    ta['Project ID'] = ta['Project ID'].combine_first(
+        ta['"Other" primary Project ID']
+    ).astype('int32')
     ta['Agency Name'] = ta['Agency Name'].combine_first(
         ta['Reporter Acronym']
     )
@@ -28,30 +27,8 @@ def load_csa():
     csa['centy'] = csa.centroid.y
     csa['name_match'] = csa['NAME'].apply(make_match_name)
     merge = pd.merge(csa, csa.bounds, how='inner', left_index=True, right_index=True)
-    return merge.drop(columns=['CSAFP', 'CBSAFP', 'AFFGEOID', 'LSAD',
+    return merge.drop(columns=['CSAFP', 'CBSAFP', 'AFFGEOID', 'LSAD', 
                                'ALAND', 'AWATER', 'geometry'])
-
-def make_json(df):
-    d = {}
-    for i, row in df.iterrows():
-        gid = row['GEOID']
-        pid = row['Project ID']
-        if gid not in d:
-            d[gid] = row[['NAME', 'centx', 'centy',
-                          'minx', 'miny', 'maxx', 'maxy']].to_dict()
-            d[gid]['agencies'] = {}
-
-        j = len(d[gid]['agencies'])
-        ta_info = row[['Agency Name', 'Reporter Acronym']].to_dict()
-        if pid:
-            d[gid]['agencies'][j] = ta_info
-        else:
-            if 'sub' not in d[gid]['agencies'][j]:
-                d[gid]['agencies'][j]['sub'] = {}
-            k = len(d[gid]['agencies'][j]['sub'])
-            d[gid]['agencies'][j]['sub'][k] = ta_info
-
-    return d
 
 def main():
     # Load the excel data:
@@ -65,13 +42,16 @@ def main():
     agencies['name_match'] = agencies['UZA Name'].apply(make_match_name)
 
     csa = load_csa()
-    merge = pd.merge(agencies, csa, how='left', on='name_match')
-    merge = merge.drop(columns=['UZA Name', 'name_match'])
+    merge = pd.merge(agencies, csa, how='inner', on='name_match')
 
-    with open('data/output/ta.json', 'w') as fp:
-        json.dump(make_json(merge), fp, indent=2)
-
-    # group.to_json('data/output/ta.json', orient='index')
+    # Export TA metadata
+    merge[['Project ID', 'Agency Name', 'Reporter Acronym', 'GEOID']].to_csv(
+        'data/output/ta.csv', index_label='id')
+    
+    # Export MSA metadata
+    geo = merge[['GEOID', 'NAME', 'centx', 'centy', 'minx', 'miny', 'maxx', 'maxy']]
+    geo = geo.groupby('GEOID').first()
+    geo.to_csv('data/output/msa.csv', index_label='id')
 
 if __name__ == "__main__":
     main()
