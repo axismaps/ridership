30b8ef5b4c96c6f41870d1fa9ec2d8ea4a182394
David Heyman <dave@axismaps.com>
2018-12-03 12:36:38 +0000

    Updated TA query

--- src/js/data/dataMethods.js
+++ src/js/data/dataMethods.js
@@ -213,7 +213,7 @@ const dataMethods = {
   getData(callback) {
     const { cleanData } = dataMethods;
     Promise.all([
-      d3.json('https://ridership.carto.com/api/v2/sql?q=SELECT+DISTINCT+ON+(taid)+*+FROM+ta'),
+      d3.json('https://ridership.carto.com/api/v2/sql?q=SELECT%20%2A%20FROM%20ta%20WHERE%20display%20%3D%20true'),
       d3.json('https://ridership.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20msa'),
       d3.json('https://ridership.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20ntd'),
       d3.json('data/states.json'),
