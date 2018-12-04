c35db6b2979b5cc7ace177f5e18e3c3934f58844
David Heyman <dave@axismaps.com>
2018-12-04 13:31:51 +0000

    Save loaded indicators to CSV

--- bin/acs.py
+++ bin/acs.py
@@ -1,3 +1,4 @@
+import os.path
 import re
 import json
 import pandas as pd
@@ -35,23 +36,34 @@ def download_census():
         acs = []
         for r in meta:
             if 'var' in r:
-                frames = []
-                for y in range(2010, 2016):
-                    for i, row in counties.iterrows():
-                        res = c.acs5.state_county_tract(r['var'], row['STATEFP'],
-                                                        row['COUNTYFP'], Census.ALL, year=y)
-                        if res:
-                            result = pd.DataFrame(res)
-                            result['GEOID'] = pd.Series(
-                                result['state'] + result['county'] + result['tract']
-                            ).astype(str)
-                            result['year'] = y
-                            out = result.set_index(['GEOID', 'year'])
-                            frames.append(out[r['var']])
-                            print 'Loaded:', r['name'], row['STATEFP'], row['COUNTYFP'], y, i
-                        else:
-                            print 'Empty:', r['name'], row['STATEFP'], row['COUNTYFP'], y, i
-                acs.append(pd.Series(pd.concat(frames), name=r['key']))
+                filename = 'data/output/census/' + r['key'] + '.csv'
+                if os.path.isfile(filename):
+                    csv = pd.read_csv(
+                        filename, dtype={'GEOID': object}
+                    ).set_index(['GEOID', 'year'])
+                    acs.append(csv)
+                    print csv
+                else:
+                    frames = []
+                    for y in range(2010, 2016):
+                        for i, row in counties.iterrows():
+                            res = c.acs5.state_county_tract(r['var'], row['STATEFP'],
+                                                            row['COUNTYFP'], Census.ALL, year=y)
+                            if res:
+                                result = pd.DataFrame(res)
+                                result['GEOID'] = pd.Series(
+                                    result['state'] + result['county'] + result['tract']
+                                ).astype(str)
+                                result['year'] = y
+                                out = result.set_index(['GEOID', 'year'])
+                                frames.append(out[r['var']])
+                                print 'Loaded:', r['name'], row['STATEFP'], row['COUNTYFP'], y, i
+                            else:
+                                print 'Empty:', r['name'], row['STATEFP'], row['COUNTYFP'], y, i
+
+                    ind = pd.Series(pd.concat(frames), name=r['key'])
+                    ind.to_csv(filename, header=r['key'])
+                    acs.append(ind)
 
         combined = pd.concat(acs, axis=1).reset_index().merge(
             geo, on='GEOID', how='inner'
