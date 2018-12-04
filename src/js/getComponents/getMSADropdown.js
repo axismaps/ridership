cf82c04cacbd38d6c837b0368953cbb3505dd932
Benjamin Wellington <bwellington@landscapemetrics.com>
2018-12-04 16:58:00 -0500

    Make msa dropdown closed on load

--- src/js/getComponents/getMSADropdown.js
+++ src/js/getComponents/getMSADropdown.js
@@ -1,7 +1,7 @@
 import MSADropdown from '../dropdown/msaDropdown';
 
 const getMSADropdown = ({ data, state }) => new MSADropdown({
-  dropdownOpen: true,
+  dropdownOpen: false,
   toggleButton: d3.select('.header__msa-dropdown-text'),
   currentMSA: state.get('msa'),
   msaList: data.get('msa'),
