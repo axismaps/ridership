6b2667f6d345dff4b11c3f0ab1a1d5fc9239de8a
Benjamin Wellington <bwellington@landscapemetrics.com>
2018-12-06 13:43:24 -0500

    Establish system for caching loaded and calculated census data

--- src/js/stateUpdate/stateUpdateMSA.js
+++ src/js/stateUpdate/stateUpdateMSA.js
@@ -4,6 +4,7 @@ const updateComponents = ({
   components,
   data,
   msa,
+  years,
 }) => {
   const {
     msaAtlas,
@@ -11,7 +12,7 @@ const updateComponents = ({
 
   const cachedTractGeoJSON = data.get('cachedTractGeoJSON');
 
-  const tractGeo = cachedTractGeoJSON.get(msa.msaId);
+  const tractGeo = cachedTractGeoJSON.get(`${msa.msaId}-${years[0]}-${years[1]}`);
   msaAtlas
     .config({
       msa,
@@ -55,84 +56,89 @@ const censusFields = [
   },
 ];
 
-const loadTractData = ({
+const processGeoJSON = ({
+  years,
   msa,
   components,
   data,
-  years,
 }) => {
-  console.log('years', years);
+  const cachedTractData = data.get('cachedTractData');
+  const { censusData, tractTopo } = cachedTractData.get(msa.msaId);
+  const tractGeoRaw = topojson.feature(
+    tractTopo,
+    tractTopo.objects[`tract-${msa.msaId}`],
+  );
+
   const cachedTractGeoJSON = data.get('cachedTractGeoJSON');
-  const getCensusTable = ({ censusData, year }) => censusData.rows
+  const getCensusTable = ({ year }) => censusData.rows
     .filter(d => d.year === year)
     .reduce((accumulator, census) => {
       accumulator[String(census.geoid)] = census;
       return accumulator;
     }, {});
 
+  const table1 = getCensusTable({
+    year: years[0],
+    censusData,
+  });
+
+  const table2 = getCensusTable({
+    year: years[1],
+    censusData,
+  });
+
+  const tractGeo = Object.assign({}, tractGeoRaw);
+  tractGeo.features = tractGeoRaw.features.map((feature) => {
+    const featureCopy = Object.assign({}, feature);
+    featureCopy.properties = Object.assign({}, feature.properties);
+
+    const census1 = table1[feature.properties.id];
+    const census2 = table2[feature.properties.id];
+
+    featureCopy.properties[`census${years[0]}`] = census1;
+    featureCopy.properties[`census${years[1]}`] = census2;
+    if (census1 === undefined || census2 === undefined) {
+      return featureCopy;
+    }
+    const censusChange = censusFields
+      .reduce((accumulator, field) => {
+        if (census1[field.value] !== 0) {
+          accumulator[field.value] = (census2[field.value] - census1[field.value])
+          / census1[field.value];
+        } else {
+          accumulator[field.value] = null;
+        }
+
+        return accumulator;
+      }, {});
+    Object.assign(featureCopy.properties, censusChange);
+    return featureCopy;
+  });
+  cachedTractGeoJSON.set(`${msa.msaId}-${years[0]}-${years[1]}`, tractGeo);
+
+  updateComponents({
+    msa,
+    components,
+    data,
+    years,
+  });
+};
+
+const loadTractData = ({
+  msa,
+  components,
+  data,
+  years,
+}) => {
   Promise.all([
     d3.json(`data/tracts/tract-${msa.msaId}.json`),
     d3.json(`https://ridership.carto.com/api/v2/sql?q=${encodeURIComponent(`SELECT * FROM census WHERE msaid = ${msa.msaId}`)}`),
   ]).then((rawData) => {
     const [tractTopo, censusData] = rawData;
-    // censusData.rows.forEach((d) => {
-    //   console.log('years', d.year, years[0]);
-    // });
-    // console.log('censusData', censusData
-    //   .rows.filter(d => d.year === years[0]));
-    const tractGeoRaw = topojson.feature(
-      tractTopo,
-      tractTopo.objects[`tract-${msa.msaId}`],
-    );
-    // const censusTable = censusData.rows.reduce((accumulator, census) => {
-    //   accumulator[String(census.geoid)] = census;
-    //   return accumulator;
-    // }, {});
-    const table1 = getCensusTable({
-      year: years[0],
-      censusData,
-    });
-
-    const table2 = getCensusTable({
-      year: years[1],
-      censusData,
-    });
-
-    const tractGeo = Object.assign({}, tractGeoRaw);
-    tractGeo.features = tractGeoRaw.features.map((feature) => {
-      const featureCopy = Object.assign({}, feature);
-      featureCopy.properties = Object.assign({}, feature.properties);
-
-      const census1 = table1[feature.properties.id];
-      const census2 = table2[feature.properties.id];
-
-      featureCopy.properties[`census${years[0]}`] = census1;
-      featureCopy.properties[`census${years[1]}`] = census2;
-      if (census1 === undefined || census2 === undefined) {
-        return featureCopy;
-      }
-      const censusChange = censusFields
-        .reduce((accumulator, field) => {
-          if (census1[field.value] !== 0) {
-            accumulator[field.value] = (census2[field.value] - census1[field.value])
-            / census1[field.value];
-          } else {
-            accumulator[field.value] = null;
-          }
-
-          return accumulator;
-        }, {});
-      Object.assign(featureCopy.properties, censusChange);
-      // const census = censusData.rows.find(d => String(d.geoid) === feature.properties.id);
-      // const census = censusTable[feature.properties.id];
-      // if (census !== undefined) {
-      //   Object.assign(featureCopy.properties, census);
-      // }
-      return featureCopy;
-    });
-    cachedTractGeoJSON.set(msa.msaId, tractGeo);
-
-    updateComponents({
+    const cachedTractData = data.get('cachedTractData');
+    cachedTractData.set(msa.msaId, { tractTopo, censusData });
+    processGeoJSON({
+      years,
       msa,
       components,
       data,
@@ -142,17 +148,25 @@ const loadTractData = ({
 
 const getStateUpdateMSA = ({ components, data }) => function updateMSA() {
   const cachedTractGeoJSON = data.get('cachedTractGeoJSON');
+  const cachedTractData = data.get('cachedTractData');
   const msa = this.get('msa');
   const years = this.get('years');
 
-  if (cachedTractGeoJSON.has(msa.msaId)) {
-    updateComponents.call(this, { components, data });
+  if (cachedTractGeoJSON.has(`${msa.msaId}-${years[0]}-${years[1]}`)) {
+    updateComponents({
+      components,
+      data,
+      msa,
+      years,
+    });
+  } else if (cachedTractData.has(msa.msaId)) {
+    processGeoJSON({
+      years,
+      msa,
+      components,
+      data,
+    });
   } else {
-    // d3.json(`data/tracts/tract-${msa.msaId}.json`)
-    //   .then((tractTopo) => {
-    //     cachedTractGeoJSON.set(msa.msaId, tractTopo);
-    //     updateComponents.call(this, { components, data });
-    //   });
     loadTractData({
       msa,
       components,
