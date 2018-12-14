63b07461d8f4097804497ed1bcb13f967f98f670
David Heyman <dave@axismaps.com>
2018-12-14 12:59:11 +0000

    Slightly better matching of TA names to routes

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -1,8 +1,32 @@
 mapshaper data/geojson/transit/**/routes.geojson combine-files \
   -merge-layers force \
-  -join data/output/ta.csv keys=operated_by_name,taname unmatched \
+  -join data/output/ta.csv keys=operated_by_name,taname \
+  -filter "taid != null" \
   -filter-fields taid,msa_color,high_frequency,name,operated_by_name,tags,vehicle_type \
+  -o data/output/routes_match_long.geojson
+
+mapshaper data/geojson/transit/**/routes.geojson combine-files \
+  -merge-layers force \
+  -join data/output/ta.csv keys=operated_by_name,tashort \
+  -filter "taid != null" \
+  -filter-fields taid,msa_color,high_frequency,name,operated_by_name,tags,vehicle_type \
+  -o data/output/routes_match_short.geojson
+
+mapshaper data/geojson/transit/**/routes.geojson combine-files \
+  -merge-layers force \
+  -join data/output/ta.csv keys=operated_by_name,taname \
+  -filter "taid == null" \
+  -filter-fields high_frequency,name,operated_by_name,tags,vehicle_type \
+  -join data/output/ta.csv keys=operated_by_name,tashort \
+  -filter "taid == null" \
+  -filter-fields taid,msa_color,high_frequency,name,operated_by_name,tags,vehicle_type \
+  -o data/output/routes_match_none.geojson
+
+mapshaper data/output/routes_match*.geojson combine-files \
+  -merge-layers force \
   -o data/output/routes.geojson
+
+rm data/output/routes_match*.geojson
 
 mapshaper data/geojson/transit/**/stops.geojson combine-files \
   -merge-layers force \
