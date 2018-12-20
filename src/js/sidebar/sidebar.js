8ee964dc4c4214e84b8fa7927b5d900e3e67ad2a
Andy Woodruff <awoodruff@gmail.com>
2018-12-20 11:08:36 -0500

    Add sidebar legends (TA list, compare list, etc.) to export

--- src/js/sidebar/sidebar.js
+++ src/js/sidebar/sidebar.js
@@ -233,6 +233,29 @@ const privateMethods = {
 
     this.updateSearchResult();
   },
+
+  getMutlilineText({
+    text,
+    maxWidth,
+    ctx,
+  }) {
+    const lines = [];
+    const words = text.split(' ');
+    words.forEach((word) => {
+      if (lines.length === 0) {
+        lines.push(word);
+      } else {
+        const newLine = `${lines[lines.length - 1]} ${word}`;
+        if (ctx.measureText(newLine).width > maxWidth && newLine.includes(' ')) {
+          lines.push(word);
+        } else {
+          lines[lines.length - 1] = newLine;
+        }
+      }
+    });
+
+    return lines;
+  },
 };
 
 class Sidebar {
@@ -409,9 +432,63 @@ class Sidebar {
       sparkRows,
       currentSidebarView,
       pcpContainer,
+      legendContainer,
+      compareContainer,
     } = privateProps.get(this);
 
+    const {
+      getMutlilineText,
+    } = privateMethods;
+
     const { SVGtoCanvas } = exportMethods;
+
+    const graphicsCanvas = document.createElement('canvas');
+    const legendCanvas = document.createElement('canvas');
+    legendCanvas.width = 420;
+    const legendCtx = legendCanvas.getContext('2d');
+    const sparklinesLegendRows = legendContainer.selectAll('.sidebar__sparkline-legend-row');
+    const compareRows = compareContainer.selectAll('.sidebar__compare-row');
+    let legendHeight = 0;
+    if (sparklinesLegendRows.size() > 0) {
+      legendCanvas.height = legendContainer.node().offsetHeight;
+      legendCtx.font = '15px Mark, Arial, sans-serif';
+      legendCtx.textBaseline = 'bottom';
+      sparklinesLegendRows.each((d) => {
+        legendCtx.fillStyle = d.color;
+        legendCtx.fillRect(0, legendHeight + 3, 12, 12);
+
+        const lines = getMutlilineText({
+          text: d.taName,
+          maxWidth: 200,
+          ctx: legendCtx,
+        });
+        legendCtx.fillStyle = '#666';
+        lines.forEach((line, lineNumber) => {
+          legendHeight += 18;
+          legendCtx.fillText(line, 32, legendHeight);
+        });
+      });
+    } else if (compareRows.size() > 0) {
+      legendCanvas.height = compareContainer.node().offsetHeight;
+      legendCtx.font = '15px Mark, Arial, sans-serif';
+      legendCtx.textBaseline = 'bottom';
+      legendCtx.fillStyle = '#666';
+      legendHeight = 18;
+      legendCtx.fillText('Comparing:', 0, legendHeight);
+      compareRows.each((d) => {
+        const lines = getMutlilineText({
+          text: d.name || d.taName,
+          maxWidth: legendCanvas.width - 40,
+          ctx: legendCtx,
+        });
+        lines.forEach((line, lineNumber) => {
+          legendHeight += 18;
+          legendCtx.fillText(line, 20, legendHeight);
+        });
+      });
+    } else {
+      legendCanvas.height = 1;
+    }
 
     if (currentSidebarView === 'sparklines') {
       const promises = [];
@@ -426,23 +503,19 @@ class Sidebar {
               rowCanvas.height = canvas.height;
               const ctx = rowCanvas.getContext('2d');
               ctx.drawImage(canvas, rowCanvas.width - canvas.width, 0);
-              ctx.font = '15px Mark, Arial, sans-serif';
-              ctx.fillStyle = '#666';
+              if (title.classed('sidebar__sparkline-title--selected')) {
+                ctx.font = 'bold 15px Mark, Arial, sans-serif';
+                ctx.fillStyle = '#2D74ED';
+              } else {
+                ctx.font = '15px Mark, Arial, sans-serif';
+                ctx.fillStyle = '#666';
+              }
               ctx.textBaseline = 'middle';
 
-              const lines = [];
-              const words = title.text().split(' ');
-              words.forEach((word) => {
-                if (lines.length === 0) {
-                  lines.push(word);
-                } else {
-                  const newLine = `${lines[lines.length - 1]} ${word}`;
-                  if (ctx.measureText(newLine).width > rowCanvas.width - canvas.width - 15 && newLine.includes(' ')) {
-                    lines.push(word);
-                  } else {
-                    lines[lines.length - 1] = newLine;
-                  }
-                }
+              const lines = getMutlilineText({
+                text: title.text(),
+                maxWidth: rowCanvas.width - canvas.width - 15,
+                ctx,
               });
 
               lines.forEach((line, i) => {
@@ -455,49 +528,60 @@ class Sidebar {
         }));
       });
 
-      const finalCanvas = document.createElement('canvas');
-      finalCanvas.width = 420;
-      finalCanvas.height = sparkRows.size() * 60;
-      const ctx = finalCanvas.getContext('2d');
+      graphicsCanvas.width = 420;
+      graphicsCanvas.height = sparkRows.size() * 60;
+      const ctx = graphicsCanvas.getContext('2d');
       return Promise.all(promises).then((results) => {
         results.forEach((rowCanvas, i) => {
           ctx.drawImage(rowCanvas, 0, i * 60);
         });
+        const finalCanvas = document.createElement('canvas');
+        finalCanvas.width = graphicsCanvas.width;
+        finalCanvas.height = legendCanvas.height + graphicsCanvas.height;
+        const finalCtx = finalCanvas.getContext('2d');
+        finalCtx.drawImage(legendCanvas, 0, 0);
+        finalCtx.drawImage(graphicsCanvas, 0, legendHeight);
         return Promise.resolve(finalCanvas);
       });
     }
 
     const svgNode = pcpContainer.select('svg').node();
     return SVGtoCanvas({ svgNode }).then((canvas) => {
-      const finalCanvas = document.createElement('canvas');
-      finalCanvas.width = 420;
-      finalCanvas.height = canvas.height;
-      const ctx = finalCanvas.getContext('2d');
-      ctx.drawImage(canvas, finalCanvas.width - canvas.width - 15, 0);
-      ctx.font = '15px Mark, Arial, sans-serif';
-      ctx.fillStyle = '#666';
+      const headerHeight = 20;
+      graphicsCanvas.width = 420;
+      graphicsCanvas.height = canvas.height + headerHeight;
+      const ctx = graphicsCanvas.getContext('2d');
+      ctx.drawImage(canvas, graphicsCanvas.width - canvas.width - 15, headerHeight);
       ctx.textBaseline = 'middle';
+      ctx.font = '12px Mark, Arial, sans-serif';
+      ctx.fillStyle = '#666';
+      ctx.textAlign = 'center';
+      ctx.fillText(d3.select('.sidebar__pcp-header').text(), graphicsCanvas.width - 0.5 * canvas.width, 18);
+      ctx.textAlign = 'left';
       pcpContainer.selectAll('.sidebar__pcp-row').each(function drawTitle(d, row) {
-        const lines = [];
-        const words = d3.select(this).select('p').text().split(' ');
-        words.forEach((word) => {
-          if (lines.length === 0) {
-            lines.push(word);
-          } else {
-            const newLine = `${lines[lines.length - 1]} ${word}`;
-            if (ctx.measureText(newLine).width > 180 && newLine.includes(' ')) {
-              lines.push(word);
-            } else {
-              lines[lines.length - 1] = newLine;
-            }
-          }
+        if (d3.select(this).classed('sidebar__pcp-row--selected')) {
+          ctx.font = 'bold 15px Mark, Arial, sans-serif';
+          ctx.fillStyle = '#2D74ED';
+        } else {
+          ctx.font = '15px Mark, Arial, sans-serif';
+          ctx.fillStyle = '#666';
+        }
+        const lines = getMutlilineText({
+          text: d3.select(this).select('p').text(),
+          maxWidth: 180,
+          ctx,
         });
-
         lines.forEach((line, i) => {
-          const y = 30 - (9 * lines.length / 2) + i * 18 + row * 60;
+          const y = headerHeight + 30 - (9 * lines.length / 2) + i * 18 + row * 60;
           ctx.fillText(line, 0, y);
         });
       });
+      const finalCanvas = document.createElement('canvas');
+      finalCanvas.width = graphicsCanvas.width;
+      finalCanvas.height = legendCanvas.height + graphicsCanvas.height;
+      const finalCtx = finalCanvas.getContext('2d');
+      finalCtx.drawImage(legendCanvas, 0, 0);
+      finalCtx.drawImage(graphicsCanvas, 0, legendHeight);
       return Promise.resolve(finalCanvas);
     });
   }
