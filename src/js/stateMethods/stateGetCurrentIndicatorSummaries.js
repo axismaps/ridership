0549c24da4b2354f38e925104ee07f539ee63fe1
Andy Woodruff <awoodruff@gmail.com>
2019-01-02 15:58:15 -0500

    Proper MSA data in sparklines (compare mode)
    #17

--- src/js/stateMethods/stateGetCurrentIndicatorSummaries.js
+++ src/js/stateMethods/stateGetCurrentIndicatorSummaries.js
@@ -1,19 +1,21 @@
 const getNationalIndicatorSummaries = function getNationalIndicatorSummaries({ data }) {
   const comparedAgencies = this.get('comparedAgencies');
   const nationalDataView = this.get('nationalDataView');
-  const ids = comparedAgencies.map(d => d[`${nationalDataView}Id`]);
   const allNationalMapData = data.get('allNationalMapData');
-  const records = allNationalMapData.map((msa) => {
+  const taRecords = allNationalMapData.map((msa) => {
     const msaRecords = msa.ta.map(ta => ta.ntd)
       .reduce((accumulator, ta) => [...accumulator, ...ta], []);
     return msaRecords;
   })
-    .reduce((accumulator, msa) => [...accumulator, ...msa], [])
-    .filter(d => ids.length === 0 || ids.includes(d[`${nationalDataView}Id`]));
+    .reduce((accumulator, msa) => [...accumulator, ...msa], []);
+  const msaRecords = allNationalMapData.map(msa => msa.ntd)
+    .reduce((accumulator, msa) => [...accumulator, ...msa], []);
   const yearRange = data.get('yearRange');
   const indicators = data.get('indicators');
   const indicatorSummaries = [];
-  {
+  if (comparedAgencies.length === 0) {
+    // to do: return national data values instead of all this
+    const records = taRecords;
     const recordsPerYear = new Map();
     for (let i = 0; i <= yearRange[1] - yearRange[0]; i += 1) {
       const year = yearRange[0] + i;
@@ -52,8 +54,37 @@ const getNationalIndicatorSummaries = function getNationalIndicatorSummaries({ d
 
       indicatorSummaries.push(indicatorCopy);
     });
+    return indicatorSummaries;
   }
+  const idField = `${nationalDataView}Id`;
+  const ids = comparedAgencies.map(d => d[idField]);
+  const records = (nationalDataView === 'ta' ? taRecords : msaRecords)
+    .filter(d => ids.includes(d[idField]));
+  indicators.forEach((indicator, key) => {
+    const indicatorCopy = Object.assign({}, indicator);
+    indicatorCopy.agencies = [];
+    const agencies = comparedAgencies.map(a => Object.assign({ summaries: [] }, a));
+    agencies.forEach((agency) => {
+      for (let i = 0; i <= yearRange[1] - yearRange[0]; i += 1) {
+        const year = yearRange[0] + i;
+        const record = records
+          .find(d => d[idField] === agency[idField]
+            && d.year === year
+            && d[key] !== null
+            && Number.isFinite(d[key]));
+        if (record !== undefined) {
+          const summary = {
+            year,
+            indicatorSummary: record[key],
+          };
+          agency.summaries.push(summary);
+        }
+      }
+      indicatorCopy.agencies.push(agency);
+    });
 
+    indicatorSummaries.push(indicatorCopy);
+  });
   return indicatorSummaries;
 };
 
