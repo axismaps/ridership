8b0ef8818e1473ce7cf11d22bec3cb610cc866c7
Andy Woodruff <awoodruff@gmail.com>
2019-01-14 11:28:39 -0500

    Update default indicator and years
    fix #26

--- src/js/getComponents/getState.js
+++ src/js/getComponents/getState.js
@@ -11,9 +11,9 @@ const getState = ({ data }) => {
     mobile: false,
     msa: null, // for msa view
     msaProbe: null, // probed msa in national view
-    indicator: data.get('indicators').get('headways'),
+    indicator: data.get('indicators').get('upt'),
     // years: data.get('yearRange'),
-    years: [2013, 2015],
+    years: [2008, 2015],
     agenciesOn: true,
     nationalDataView: 'ta', // ta or msa
     scale: 'national', // national or msa,
