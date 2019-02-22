e4db0a68fa0ef5cdd2648801f30b56400ce7f1a0
Andy Woodruff <awoodruff@gmail.com>
2019-02-22 10:15:37 -0500

    MSA defaults to population density
    fix #49

--- src/js/getComponents/getState.js
+++ src/js/getComponents/getState.js
@@ -28,7 +28,7 @@ const getState = ({ data }) => {
     agenciesOn: true,
     nationalDataView: 'ta', // ta or msa
     scale: 'national', // national or msa,
-    censusField: { text: 'Median Household Income', value: 'income' },
+    censusField: { text: 'Population Density', value: 'density' },
     distanceFilter: null,
     highlightedAgencies: [], // agencies highlighted on map/histogram/chart(s) mouseover,
     highlightedTracts: [], // tracts highlighted on map/histogram/chart mouseover
