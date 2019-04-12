ae93dfef277aa28a046e41cdd07e2d88d25a0321
David Heyman <dave@axismaps.com>
2019-04-12 11:48:25 +0100

    Small change to deploy path

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 npm run build
 aws s3 sync dist/ s3://ridership.axismaps.io --delete
-aws cloudfront create-invalidation --distribution-id E3OICTR2L3KDJ4 --paths /\*
+aws cloudfront create-invalidation --distribution-id E3OICTR2L3KDJ4 --paths "/*"
