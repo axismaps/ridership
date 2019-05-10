9565d89373ff46cae97cd9e7446836825552f1c3
David Heyman <dave@axismaps.com>
2019-05-10 15:43:36 -0500

    Deploy parameters for live and dev

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,7 @@
 npm run build
-aws s3 sync dist/ s3://ridership.axismaps.io --delete
-aws cloudfront create-invalidation --distribution-id E3OICTR2L3KDJ4 --paths "/*"
+if [ "$1" == live ]; then
+  aws s3 sync dist/ s3://ridership.axismaps.io --delete
+  aws cloudfront create-invalidation --distribution-id E3OICTR2L3KDJ4 --paths "/*"
+else
+  aws s3 sync dist/ s3://ridership-dev.axismaps.io --delete
+fi
