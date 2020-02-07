e406712ef40a03474023e5f9cb1a627a0f4b4b09
David Heyman <dave@axismaps.com>
2020-02-07 14:55:09 +0000

    Fixes for relative URLs

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@ if [ "$1" == live ]; then
   aws s3 sync dist/ s3://ridership.axismaps.io --delete
   aws cloudfront create-invalidation --distribution-id E3OICTR2L3KDJ4 --paths "/*"
 else
-  aws s3 sync dist/ s3://ridership-dev.axismaps.io --delete
+  aws s3 sync dist/ s3://ridership-dev.axismaps.io/$1 --delete
 fi
