44c96690d425eab90803edad68713440d0263531
Andy Woodruff <awoodruff@gmail.com>
2020-02-10 12:23:12 -0500

    Consolidate census list into rows

--- src/js/dropdown/indicatorDropdownMethods.js
+++ src/js/dropdown/indicatorDropdownMethods.js
@@ -3,19 +3,52 @@ const pureMethods = {
     indicators,
     contentContainer,
     updateIndicator,
+    censusFields,
   }) {
     const indicatorList = Array.from(indicators.values());
 
-    const indicatorRows = contentContainer
-      .selectAll('.indicator-dropdown__content-row')
-      .data(indicatorList)
-      .enter()
-      .append('div')
-      .attrs({
-        class: 'indicator-dropdown__content-row',
-      })
-      .text(d => d.text)
-      .on('click', updateIndicator);
+    let indicatorRows;
+
+    if (!censusFields) {
+      indicatorRows = contentContainer
+        .selectAll('.indicator-dropdown__content-row')
+        .data(indicatorList)
+        .enter()
+        .append('div')
+        .attrs({
+          class: 'indicator-dropdown__content-row',
+        })
+        .text(d => d.text)
+        .on('click', updateIndicator);
+    } else {
+      indicatorRows = contentContainer
+        .selectAll('.census-dropdown__content-row')
+        .data(censusFields)
+        .enter()
+        .append('div')
+        .attrs({
+          class: 'census-dropdown__content-row',
+        });
+
+      indicatorRows
+        .append('div')
+        .attr('class', 'census-dropdown__content-row__title')
+        .text(d => d.text);
+
+      indicatorRows
+        .append('div')
+        .datum((d, i) => indicators[i * 2])
+        .attr('class', 'census-dropdown__content-row__indicator')
+        .text('Nominal')
+        .on('click', updateIndicator);
+
+      indicatorRows
+        .append('div')
+        .datum((d, i) => indicators[i * 2 + 1])
+        .attr('class', 'census-dropdown__content-row__indicator')
+        .text('Change')
+        .on('click', updateIndicator);
+    }
 
     return indicatorRows;
   },
@@ -57,9 +90,17 @@ const pureMethods = {
     indicatorRows,
     mobileSelect,
     indicator,
+    censusFields,
   }) {
-    indicatorRows
-      .classed('indicator-dropdown__content-row--highlighted', d => (indicator === null ? false : d.id === indicator.id));
+    if (!censusFields) {
+      indicatorRows
+        .classed('indicator-dropdown__content-row--highlighted', d => (indicator === null ? false : d.id === indicator.id));
+    } else {
+      indicatorRows
+        .classed('census-dropdown__content-row--highlighted', d => (indicator === null ? false : d.value === indicator.value));
+      indicatorRows.selectAll('.census-dropdown__content-row__indicator')
+        .classed('census-dropdown__content-row__indicator--highlighted', d => (indicator === null ? false : d.id === indicator.id));
+    }
     if (mobileSelect.size()) {
       const selectNode = mobileSelect.node();
       selectNode.value = indicator === null ? null : indicator.value;
