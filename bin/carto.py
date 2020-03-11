0e11a83a0ba6ae7e73cfcc75e1e2cfa0e0f5e180
David Heyman <dave@axismaps.com>
2020-03-11 07:31:23 +0000

    Updated carto scripts to default to dev tables

--- bin/carto.py
+++ bin/carto.py
@@ -1,3 +1,7 @@
+"""
+Functions for uploading data to CARTO platform
+"""
+
 from os import path
 import gzip
 import shutil
@@ -5,25 +9,29 @@ import requests
 import settings
 
 def get_file_loc(filename):
+    """Get full file path from file name"""
     return path.join(path.dirname(__file__), '../data/output/', filename)
 
 def delete_table(name):
+    """Delete all records from table before repopulating"""
     url = 'http://' + settings.CARTO_USER + '.carto.com/api/v2/sql/'
     querystring = {'q':'DELETE FROM ' + name, 'api_key': settings.CARTO_API}
     response = requests.get(url, params=querystring)
 
-    print 'Delete result: ' + response.text
+    print('Delete result: ' + response.text)
 
 def gzip_file(filename):
+    """Compress CSV file before uploading"""
     f = get_file_loc(filename)
     with open(f, 'rb') as f_in:
         gz = f + '.gz'
         with gzip.open(gz, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
-            print 'Zipped ' + gz
+            print('Zipped ' + gz)
             return gz
 
 def copy_table(name, cols, filename):
+    """Upload new data to CARTO"""
     url = 'http://' + settings.CARTO_USER + '.carto.com/api/v2/sql/copyfrom'
     querystring = {
         'q':'COPY ' + name + ' (' + ','.join(cols) + ') FROM stdin WITH (FORMAT csv, HEADER true)',
@@ -36,10 +44,13 @@ def copy_table(name, cols, filename):
     csv = open(get_file_loc(filename), mode='rb').read()
     response = requests.post(url, headers=headers, params=querystring, data=csv)
 
-    print 'Copy result: ' + response.text
+    print('Copy result: ' + response.text)
 
-def replace_data(name, cols, filename):
+def replace_data(name, cols, filename, dev=True):
+    """Full workflow for compressing, deleting, and uploading new data"""
     gz = gzip_file(filename)
+    if dev:
+        name = name + '_dev'
     delete_table(name)
     copy_table(name, cols, gz)
-    print 'Upload to Carto was successful'
+    print('Upload to Carto was successful')
