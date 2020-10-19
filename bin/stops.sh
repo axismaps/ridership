0089a6c66f536e4e10187afa9fd93f1baa7a52c2
David Heyman <dave@axismaps.com>
2020-10-19 19:50:02 +0100

    Fixes #98

--- bin/stops.sh
+++ bin/stops.sh
@@ -3,10 +3,12 @@ mapshaper data/geojson/transit/**/stops.geojson combine-files \
   -filter-fields high_frequency,name,operators_serving_stop \
   -each 'for (var i = 0; i <= 4; i++) { this.properties["op" + i] = "99999999" }' \
   -each 'operators_serving_stop.forEach((o, i) => { this.properties["op" + i] = o.operator_name })' \
+  -each 'lng = this.x' \
+  -each 'lat = this.y' \
   -join data/meta/ta_names.csv keys=op0,name1 unmatched \
-  -o data/output/stops-unmatched.geojson target=unmatched \
+  -o data/output/stops-unmatched.geojson target=unmatched force \
   -filter "taid !== null" \
-  -o data/output/stops-matched.geojson
+  -o data/output/stops-matched.geojson force
 
 for o in {0..4}
   do
@@ -23,7 +25,7 @@ for o in {0..4}
 
 mapshaper data/output/stops-*.geojson combine-files \
   -merge-layers force \
-  -filter remove-empty \
+  -points x=lng y=lat \
   -filter-fields high_frequency,name,taid \
   -each 'this.properties.taid = this.properties.taid || 9999' \
   -o data/output/stops.geojson
@@ -33,4 +35,4 @@ rm data/output/stops-*.geojson
 mapshaper data/output/stops.geojson \
   -filter "high_frequency === true" remove-empty \
   -proj EPSG:3395 \
-  -o data/output/stops_high_frequency.geojson
+  -o data/output/stops_high_frequency.geojson force
